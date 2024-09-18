341f266d233d66686ec3cff86a3316d8de929d75
Sarib Ali <sarib95965@gmail.com>
2024-09-19 00:35:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -135,4 +135,4 @@ def predict():
         return render_template('index.html', prediction=f'Error: {str(e)}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
